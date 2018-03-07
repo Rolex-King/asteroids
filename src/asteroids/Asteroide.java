16ceea3410f001b343c446b9b0086188a0fd8613
JavierGoRa <35291605+JavierGoRa@users.noreply.github.com>
2018-03-07 11:33:14 +0100

    Update and rename asteroide.java to Asteroide.java

---
+++ src/asteroids/Asteroide.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package asteroids;
+
+/**
+ *
+ * @author Javier
+ */
+import java.util.Random;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Polygon;
+
+/**
+ *
+ * @author Javier
+ */
+
+
+
+public class Asteroide {
+    int SCENE_TAM_X = 800;
+    int SCENE_TAM_Y = 600;
+
+    Pane root;
+    
+    double asteroideX;
+    double asteroideY;
+    
+    void asteroideObj(Pane root){	
+        Polygon asteroide = new Polygon();
+        asteroide.getPoints().addAll(new Double []{
+            0.0, 0.0,
+            40.0, 20.0,
+            60.0, 60.0,
+            10.0, 60.0
+        });
+        asteroide.setFill(Color.RED);
+        root.getChildren().add(asteroide);
+        Random randomAsteroide = new Random();
+        asteroideX = randomAsteroide.nextInt(800);
+        asteroideY = randomAsteroide.nextInt(600);
+        asteroide.setTranslateX(asteroideX);
+        asteroide.setTranslateY(asteroideY);
+    }
+}
